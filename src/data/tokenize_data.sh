79dcce3afa2a49b2e2e487fd65453d0e5252cf6d
tpavankalyan <tankalapavankalyan@gmal.com>
2025-04-07 08:44:11 +0000

    first training run code

--- src/data/tokenize_data.sh
+++ src/data/tokenize_data.sh
@@ -1,7 +1,1 @@
-python /datadrive/pavan/lm/gpt-neox/tools/datasets/preprocess_data_with_chat_template.py \
-            --input "/datadrive/pavan/CurLL/data/age_0_5/chat_template_neo_x/conv_chat_format_neox.jsonl","/datadrive/pavan/CurLL/data/age_0_5/chat_template_neo_x/poem_chat_format_neox.jsonl","/datadrive/pavan/CurLL/data/age_0_5/chat_template_neo_x/story_chat_format_neox.jsonl"\
-            --jsonl-keys chat \
-            --no-mask \
-            --output /datadrive/pavan/CurLL/data/age_0_5/tokenized/ \
-            --tokenizer-path "deepseek-ai/DeepSeek-V3-0324" \
-            --dataset-impl mmap
+python3 /datadrive/pavan/CurLL/nanotron/tools/preprocess_data.py --tokenizer-name-or-path meta-llama/Meta-Llama-3-8B --output-folder /datadrive/pavan/CurLL/data/age_0_5/age_0_5_text --n-tasks 16 hf --dataset Pavankalyan/age_0_5_text
