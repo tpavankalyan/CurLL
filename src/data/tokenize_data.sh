5de95566797b448cdd78b2314ac89663530cddb8
tpavankalyan <tankalapavankalyan@gmal.com>
2025-05-14 03:20:50 +0000

    added data files

--- src/data/tokenize_data.sh
+++ src/data/tokenize_data.sh
@@ -2,3 +2,8 @@ python3 /datadrive/pavan/CurLL/nanotron/tools/preprocess_data.py --tokenizer-nam
 
 
 # CUDA_DEVICE_MAX_CONNECTIONS=1 torchrun --nproc_per_node=4 /datadrive/pavan/CurLL/nanotron/run_train.py --config-file /datadrive/pavan/CurLL/nanotron/examples/config_tiny_llama.yaml > output.txt
+
+
+## Tinystories tokenization
+#python3 /datadrive/pavan/CurLL/nanotron/tools/preprocess_data.py --tokenizer-name-or-path HuggingFaceTB/SmolLM-360M --output-folder /datadrive/pavan/CurLL/data/tinystories/train --n-tasks 16 hf --dataset roneneldan/TinyStories --split train > /datadrive/pavan/CurLL/data/tinystories/train_terminal.txt
+#python3 /datadrive/pavan/CurLL/nanotron/tools/preprocess_data.py --tokenizer-name-or-path HuggingFaceTB/SmolLM-360M --output-folder /datadrive/pavan/CurLL/data/tinystories/val --n-tasks 16 hf --dataset roneneldan/TinyStories --split validation > /datadrive/pavan/CurLL/data/tinystories/val_terminal.txt
